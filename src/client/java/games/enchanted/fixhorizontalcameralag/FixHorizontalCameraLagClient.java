4de4b8057e4fc46252e2c07068225d0c6b5433cd
ioblackshaw <58823778+Enchanted-Games@users.noreply.github.com>
2024-06-13 00:24:00 +0100

    Initial commit

---
+++ src/client/java/games/enchanted/fixhorizontalcameralag/FixHorizontalCameraLagClient.java
@@ -0,0 +1,10 @@
+package games.enchanted.fixhorizontalcameralag;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FixHorizontalCameraLagClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
