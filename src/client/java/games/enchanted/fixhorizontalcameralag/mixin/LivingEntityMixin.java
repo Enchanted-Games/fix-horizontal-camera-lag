d7936fc603b7d0d322688675cf7032a13fe5c5e7
ioblackshaw <58823778+Enchanted-Games@users.noreply.github.com>
2024-06-15 17:18:23 +0100

    clean up code, only apply fix to players

--- src/client/java/games/enchanted/fixhorizontalcameralag/mixin/LivingEntityMixin.java
+++ src/client/java/games/enchanted/fixhorizontalcameralag/mixin/LivingEntityMixin.java
@@ -1,36 +1,19 @@
 package games.enchanted.fixhorizontalcameralag.mixin;
 
-import com.mojang.blaze3d.platform.Window;
-import net.minecraft.client.Camera;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.MouseHandler;
-import net.minecraft.client.player.AbstractClientPlayer;
-import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.entity.BoatRenderer;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.*;
-import net.minecraft.world.entity.decoration.HangingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelReader;
-import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
-public class ExampleClientMixin extends Entity {
+public class LivingEntityMixin extends Entity {
     @Inject(
         at = @At("HEAD"),
         method = "getViewYRot(F)F",
@@ -38,10 +21,12 @@ public class ExampleClientMixin extends Entity {
     )
     // use yRot instead of yHeadRot for view rotation
 	public void getViewYRot(float d, CallbackInfoReturnable<Float> cir) {
-        cir.setReturnValue(d == 1.0F ? this.yRotO : Mth.lerp(d, this.yRotO, this.getYRot()));
+        if((Entity)this instanceof Player) {
+            cir.setReturnValue(d == 1.0F ? this.yRotO : Mth.lerp(d, this.yRotO, this.getYRot()));
+        }
 	}
 
-    public ExampleClientMixin(EntityType<?> entityType, Level level) {
+    public LivingEntityMixin(EntityType<?> entityType, Level level) {
         super(entityType, level);
     }
 
