875511a7f83d2b84d8bc7ae079f1fc92d5954ae7
ioblackshaw <58823778+Enchanted-Games@users.noreply.github.com>
2024-08-23 21:09:05 +0100

    fix insane camera jittering while in a boat

--- common/src/main/java/games/enchanted/fixhorizontalcameralag/mixin/LocalPlayerMixin.java
+++ common/src/main/java/games/enchanted/fixhorizontalcameralag/mixin/LocalPlayerMixin.java
@@ -5,6 +5,7 @@ import games.enchanted.fixhorizontalcameralag.config.ConfigHandler;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.util.Mth;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -15,11 +16,12 @@ public abstract class LocalPlayerMixin extends AbstractClientPlayer {
         super(clientLevel, gameProfile);
     }
 
-    @Redirect(method = "getViewYRot(F)F", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;isPassenger()Z"))
-    public boolean pretendPlayerIsNotAPassengerWhenGettingViewYRot(LocalPlayer instance) {
+    @Redirect(method = "getViewYRot(F)F", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/AbstractClientPlayer;getViewYRot(F)F"))
+    public float getYRotInsteadOfYHeadRot(AbstractClientPlayer instance, float delta) {
         if(ConfigHandler.INSTANCE.isModEnabled.getValue()) {
-            return false;
+            return delta == 1.0F ? this.yRotO : Mth.lerp(delta, this.yRotO, this.getYRot());
+        } else {
+            return super.getViewYRot(delta);
         }
-        return this.isPassenger();
     }
 }
