f9cfde0c75ffd0ba33ad7e9fc475381790bfc06a
ioblackshaw <isaacoblackshaw@gmail.com>
2026-03-30 00:02:29 +0100

    fix neoforge config, fix dist

--- src/main/java/games/enchanted/eg_fix_horizontal_camera_lag/neoforge/NeoForgeEntry.java
+++ src/main/java/games/enchanted/eg_fix_horizontal_camera_lag/neoforge/NeoForgeEntry.java
@@ -3,9 +3,11 @@
 
 import games.enchanted.eg_fix_horizontal_camera_lag.common.ModConstants;
 import games.enchanted.eg_fix_horizontal_camera_lag.common.ModEntry;
+import games.enchanted.eg_fix_horizontal_camera_lag.common.config.ConfigScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.ConfirmScreen;
 import net.minecraft.network.chat.Component;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
@@ -13,14 +15,13 @@ import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 /^*
  * This is the entry point for your mod's neoforge side.
  ^/
-@Mod(ModConstants.MOD_ID)
+@Mod(value = ModConstants.MOD_ID, dist = Dist.CLIENT)
 public class NeoForgeEntry {
     public NeoForgeEntry() {
         ModEntry.init();
 
         ModLoadingContext.get().registerExtensionPoint(
-            IConfigScreenFactory.class, () -> (client, parent) ->
-                new ConfirmScreen(b -> Minecraft.getInstance().setScreen(parent), Component.literal(ModConstants.MOD_NAME + " Config Placeholder"), Component.empty())
+            IConfigScreenFactory.class, () -> (client, parent) -> ConfigScreen.createConfigScreen(parent)
         );
     }
 }
